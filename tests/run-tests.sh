576e2d973c698b41634b03d2136a5ee95550ff78
Jinwei Zhao <clarkzjw@uvic.ca>
2025-04-16 12:31:56 -0700

    add test sample data and add tests to github actions (#7)
    * add test files
    
    * update github actions with tests

---
+++ tests/run-tests.sh
@@ -0,0 +1,13 @@
+docker run -d \
+  -v ./sample-data:/app/starlink/data \
+  clarkzjw/leoviz:starlink \
+  poetry run python3 plot.py --lat 48 --lon -123 --id 2025-04-15-01-31-27
+
+sleep 120
+
+stat ./sample-data/starlink-2025-04-15-01-31-27.mp4
+
+if [ $? -ne 0 ]; then
+  echo "Error: Video file not found or not created."
+  exit 1
+fi
